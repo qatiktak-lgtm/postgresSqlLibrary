9ba82406c3e3dc11f8df4e292c1cfcaa16564ca6
Arkadii <qatiktak@gmail.com>
2026-06-30 20:36:37 +0200

    add-3 new file

---
+++ update_queries-01.sql
@@ -0,0 +1,11 @@
+-- Например увеличиваем цену книги
+UPDATE books
+SET price = price + 5.00
+WHERE id = 2;
+
+
+-- Увеличить цену всех товаров на 10%
+UPDATE products
+SET price = price * 1.10;
+
+-- Here's this new book request
