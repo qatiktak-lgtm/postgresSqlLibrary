7069cb857feb8a2fcabc27975bcba1b24205a095
Arkadii <qatiktak@gmail.com>
2026-06-30 21:17:06 +0200

    add-5 Changed project description-2 and added a file with a new update-2

---
+++ queries/update_queries-02.sql
@@ -0,0 +1,26 @@
+UPDATE users
+SET name = 'Alex Johnson'
+WHERE id = 1;
+
+-- Изменить email пользователя
+UPDATE users
+SET email = 'alex.johnson@example.com'
+WHERE id = 1;
+
+-- Увеличить цену книги
+UPDATE books
+SET price = price + 5.00
+WHERE id = 2;
+
+
+
+
+-- Добавляем в проект еще один запрос Увеличить цену книги
+UPDATE books
+SET price = price * 1.50
+WHERE id = 3;
+
+-- По замечанию рецензента даем новое имя
+UPDATE users
+SET name = 'John Alexson'
+WHERE id = 99;
